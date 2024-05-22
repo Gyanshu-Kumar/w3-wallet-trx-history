1986854eebd406762edf3cbe056e835f19fe742a
Gyanshu-kumar <gyanshusingh152@gmail.com>
2024-05-22 23:58:33 +0530

    commit

--- server.js
+++ server.js
@@ -2,24 +2,20 @@ const express = require("express");
 const Moralis = require("moralis").default;
 const app = express();
 const cors = require("cors");
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/txs", async (req, res) => {
-  
   try {
-     const { query } = req;
-
+    const { query } = req;
     const balance = await Moralis.EvmApi.transaction.getWalletTransactions({
-
       address: query.address,
       chain: query.chain,
     });
-
     const result = balance.raw;
-
     return res.status(200).json({ result });
   } catch (e) {
     console.log(e);
@@ -32,6 +28,8 @@ Moralis.start({
   apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjdiYTM3OTQ1LWI3NWItNDQ1Mi05ZGVkLTEyMWIwOWFiODJlOCIsIm9yZ0lkIjoiMzkzMjc4IiwidXNlcklkIjoiNDA0MTA5IiwidHlwZUlkIjoiODMyY2RlOGEtZTIxYS00ZGM0LTg3ZjQtOWY1YWQ4OWQ3YmU2IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MTYzNzI4OTYsImV4cCI6NDg3MjEzMjg5Nn0.DO8fCYyRBv5lQmTMlYfuLribIMmECmZWDucazIWYUuk",
 }).then(() => {
   app.listen(port, () => {
-    console.log(`Listening for API Calls`);
+    console.log(`Listening for API Calls on port ${port}`);
   });
 });
+
+module.exports = app;
